23b01f540c21dd7075b5740811a2238d1f8a040b
Rickelme Maia <rickelmevgta@gmail.com>
2022-03-14 10:38:58 -0300

    portfolio finalizado

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,13 +4,10 @@ import styles from "./NavBar.modules.css"
 function NavBar(){
     return(
         <div className="navBar">
-            
-           
                 <a href="#aboutMe">About Me</a>
                 <a href="#services">Services</a>
                 <a href="#projects">Projects</a>
                 <a href="#contact">Contact</a>
-          
         </div>
     )
 }
